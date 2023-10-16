347e58240a17979a9c25907faff9aef77300cdeb
unknown <morganhezekiah111@gmail.com>
2023-10-16 11:37:03 +0100

    'g'

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import time
+import requests
+import json
 
 
+
+API = "https://jsonplaceholder.typicode.com/todos/1"
 def runner():
     print("This is our scheduled task runnif")
+    data = requests.get(API)
+    print(json.loads(data.content))
 
 
 
